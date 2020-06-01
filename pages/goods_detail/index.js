1b04eaee72e492ee9a7197aad0997997fcf00abf
lianghuikun <906630008@qq.com>
2020-06-01 20:59:09 +0800

    轮播图

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,29 @@
-// pages/goos_detail/index.js
+import { request } from "../../request/index";
+import regeneratorRuntime from "../../lib/runtime/runtime";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsObj:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options);
+    // console.log(options);
+    const { goods_id } = options;
+    this.getGoodsDetail(goods_id)
+  },
+  // 获取商品详情
+  async getGoodsDetail(goods_id) {
+    const goodsObj = await request({ url: "/goods/detail", data: { goods_id } });
+    // console.log(res);
+    this.setData({
+      goodsObj
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
