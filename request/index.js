d33d6cb76b7d0ee2c6a732fe6a0cac1d0129fa88
lianghuikun <906630008@qq.com>
2020-06-01 20:10:43 +0800

    请求加loading

--- request/index.js
+++ request/index.js
@@ -1,5 +1,13 @@
 // 用es6的promise来优化 wx-request
+// 同时发送异步代码的次数
+let ajaxTimes = 0;
 export const request=(params)=>{
+    ajaxTimes++;
+    // 显示加载中效果
+    wx.showLoading({
+        title: "加载中",
+        mask: true,// 魔态
+    });
     // 定义公共的url
     const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1";
     // resolve成功时的回调函数，reject失败时的回调函数
@@ -13,6 +21,13 @@ export const request=(params)=>{
         },
         fail:(error)=>{
             reject(error);
+        },
+        complete:()=>{
+            ajaxTimes--;
+            if (ajaxTimes ===0) {
+                // 关闭加载中
+                wx.hideLoading();
+            }
         }
        });
     })
