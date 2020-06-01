659c3a43e55747d2d5ac5871c7c5db1451649cba
lianghuikun <906630008@qq.com>
2020-06-01 21:30:53 +0800

    详情

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -22,7 +22,17 @@ Page({
     const goodsObj = await request({ url: "/goods/detail", data: { goods_id } });
     // console.log(res);
     this.setData({
-      goodsObj
+      goodsObj:{
+        goods_name:goodsObj.goods_name,
+        goods_price:goodsObj.goods_price,
+        // goods_introduce:goodsObj.goods_introduce,
+        // iphone部分收集，不识别webp格式图片
+        // 最好找到后台工程师，让后台修改
+        // 前端临时修改，要确保后台存在 1.webp  =》1.jpg 
+        // 全部替换（不推荐)
+        goods_introduce:goodsObj.goods_introduce.replace(/\.webp/g,'.jpg'),
+        pics:goodsObj.pics
+      }
     });
   },
 
