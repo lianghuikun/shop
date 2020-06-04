139db8ec996c06703691de7369b81b5d786033fc
lianghuikun <906630008@qq.com>
2020-06-04 21:30:44 +0800

    获取收货地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,66 +1,82 @@
-// pages/cart/index.js
+/**
+ * 一.获取用户的收货地址
+ *  1.绑定事件
+ *  2.调用小程序内置api获取用户的收货地址
+ *  3.获取用户对小程序锁获取的收货地址的权限状态scope
+ *    1）假设用户点击获取收货地址的提示框， 点击确定 scope为true,可以直接调用获取收货地址方法
+ *    2）加入点击取消则 scope为false，则需要诱导用户自己打开授权设置页面，当用户重新授予获取收货地址权限时获取
+ *    3）假设用户从来没调用过收货地址，则scope为undefined，可以直接调用收货地址
+ *   4.把获取到的收货地址放入本地存储中
+ */   
+import {getSetting, chooseAddress,openSetting} from "../../utils/asyncWx.js";
+
+import regeneratorRuntime from "../../lib/runtime/runtime";
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  // 优化后
+  // 点击收货地址
+  async handleChooseAddress(){
+    try {
+    // console.log("点击收货地址")
+    // 1获取权限状态
+    const res1 = await getSetting();
+    const scopeAddress = res1.authSetting["scope.address"];
+    // 2.判断权限状态
+    if (scopeAddress===false){
+      // 3.诱导用户授权,如果未授权，需要授权
+      await openSetting();
+    }
+     // 3.调用获取收货地址
+     const address = await chooseAddress();
+    //  console.log(address); 
+    // 4.存入缓存中
+     wx.setStorage({
+       key: 'address',
+       data: address
+     });
+       
+    } catch(error) {
+      console.log(error);
+    }
+    
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 优化前
+  // 点击收货地址
+  handleChooseAddress2(){
+    // console.log("点击收货地址")
+    // 1获取权限状态
+    wx.getSetting({
+      success: (result) => {
+        // 2.获取权限状态
+        const scopeAddress = result.authSetting["scope.address"];
+        if (scopeAddress===true||scopeAddress===undefined){
+          // 如果时第一打开或者之前授权过，则调用地址
+          wx.chooseAddress({
+            success: (res) => {
+              console.log(res);
+            }
+          });
+        }else {
+          // 用户以前拒绝过授予地址访问的权限，则诱导用户授权
+          wx.openSetting({
+            success: (res) => {
+              // 获得授权之后调用收货地址
+              wx.chooseAddress({
+                success: (result3) => {
+                  console.log(result3);
+                }
+              });
+                
+            },
+            fail: () => {},
+            complete: () => {}
+          });
+            
+        }
+      },
+      fail: () => {},
+      complete: () => {}
+    });
+      
+      
   }
 })
---
+++ utils/asyncWx.js
@@ -0,0 +1,54 @@
+/**
+ * 封装微信的api接口：
+ *  getSetting
+ */
+export const getSetting=()=>{
+    return new Promise((resolve, reject)=>{
+        wx.getSetting({
+            success: (result) => {
+                resolve(result);
+            },
+            fail: (err) => {
+                reject(err);
+            }
+        });
+          
+    });
+}
+
+
+/**
+ * 封装微信的api接口：
+ *  chooseAddress
+ */
+export const chooseAddress=()=>{
+    return new Promise((resolve, reject)=>{
+        wx.chooseAddress({
+            success: (result) => {
+                resolve(result);
+            },
+            fail: (err) => {
+                reject(err);
+            }
+        });
+          
+    });
+}
+
+/**
+ * 封装微信的api接口：
+ *  openSetting
+ */
+export const openSetting=()=>{
+    return new Promise((resolve, reject)=>{
+        wx.openSetting({
+            success: (result) => {
+                resolve(result);
+            },
+            fail: (err) => {
+                reject(err);
+            }
+        });
+          
+    });
+}
